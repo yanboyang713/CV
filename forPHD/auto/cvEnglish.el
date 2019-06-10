c2f6bf1207a8c683cb36cf80abab38e6418e419e
Boyang <yanboyang713@gmail.com>
2019-06-10 16:44:06 +1000

    new

--- forPHD/auto/cvEnglish.el
+++ forPHD/auto/cvEnglish.el
@@ -1,12 +1,12 @@
 (TeX-add-style-hook
  "cvEnglish"
  (lambda ()
-   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "hyperref")
-   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "hyperimage")
-   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "hyperbaseurl")
-   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "nolinkurl")
-   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "url")
    (add-to-list 'LaTeX-verbatim-macros-with-braces-local "path")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "url")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "nolinkurl")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "hyperbaseurl")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "hyperimage")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "hyperref")
    (add-to-list 'LaTeX-verbatim-macros-with-delims-local "path")
    (TeX-run-style-hooks
     "latex2e"
@@ -20,6 +20,7 @@
    (LaTeX-add-bibliographies
     "bibliography")
    (LaTeX-add-xcolor-definecolors
+    "myblue"
     "pblue"))
  :latex)
 
